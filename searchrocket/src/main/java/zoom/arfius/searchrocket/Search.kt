a8edf5b2f99afc0b563729b455adc14767c4c5ec
alfarruggia <45227545+alfarruggia@users.noreply.github.com>
2019-09-25 08:50:01 +0100

    fix module import

---
+++ searchrocket/src/main/java/zoom/arfius/searchrocket/Search.kt
@@ -0,0 +1,12 @@
+package zoom.arfius.searchrocket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Search : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
