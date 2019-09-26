810fd28ee08125c61319b62287c89dbf552f5934
alfarruggia <45227545+alfarruggia@users.noreply.github.com>
2019-09-26 08:33:36 +0100

    animation

--- orientation/src/main/java/zoom/arfius/orientation/Orientation.kt
+++ orientation/src/main/java/zoom/arfius/orientation/Orientation.kt
@@ -12,37 +12,46 @@ import zoom.arfius.orientation.presenter.OPresenter
 class Orientation : AppCompatActivity(), IOrientation {
 
     lateinit var presenter: OPresenter
+    lateinit var views:Array<TextView>
 
     override fun updateText(value:Float) {
         axisx.setText(""+value);
-        if(value>0){
-            update(textViewH,5.0,value)
-            update(textViewE,4.0,value)
-            update(textViewL1,3.0,value)
-            update(textViewL2,1.0,value)
-            update(textViewO,1.0,value)
-        }else
-        {
-            update(textViewH,1.0,-1*value)
-            update(textViewE,2.0,-1*value)
-            update(textViewL1,3.0,-1*value)
-            update(textViewL2,4.0,-1*value)
-            update(textViewO,5.0,-1*value)
+
+        if(value>=-40.0&&value<-24.0){
+            update(textViewH,50.0)
+
         }
-
-
+        if(value>=-24.0&&value<-8.0){
+            update(textViewE,50.0)
+        }
+        if(value>=-8.0&&value<8.0){
+            update(textViewL1,50.0)
+        }
+        if(value>=8.0&&value<24.0){
+            update(textViewL2,50.0)
+        }
+        if(value>=24.0&&value<40.0){
+            update(textViewO,50.0)
+        }
     }
 
-    fun update(tw:TextView,weight:Double,scale:Float ){
-        var c=weight*scale+24;
+    fun update(tw:TextView,size:Double ){
+        var c=size;
         Log.i("update",tw.text.toString()+" >"+c);
         tw.textSize= c.toFloat()
+        views.forEach { e->
+
+            if(e.id!=tw.id)
+                e.textSize=24.0F;
+
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orientation)
         presenter= OPresenter(this)
+        views=arrayOf(textViewH,textViewE,textViewL1,textViewL2,textViewO)
     }
 
     override fun onResume() {
